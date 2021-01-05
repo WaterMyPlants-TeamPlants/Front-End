b3bff8ffe0362f412d58425046fc74b0322937fc
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2021-01-05 11:41:03 -0500

    added save button

--- src/components/edit-user.js
+++ src/components/edit-user.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Styled from "styled-components";
+import axios from "axios";
 
+//imported images
 import pencil from "../logos/pencil.png";
 import user from "../logos/User.png";
 
 
-const UserValues = { //can use user values here instead of empty strigns
-    username: "hairo",
+const UserInfo = {
+    username: "",
     email: "",
     phone: "",
     notifications: "",
@@ -14,7 +16,7 @@ const UserValues = { //can use user values here instead of empty strigns
 
 
 const EditUser = () => {
-    const [edit, setEdit] = useState(UserValues)
+    const [edit, setEdit] = useState(UserInfo)
 
     const Change = (evt) => {
         const correctValue = evt.target.type === "checkbox" ? evt.target.checked : 
@@ -23,10 +25,22 @@ const EditUser = () => {
         console.log(correctValue)
     }
 
+    const Save = (evt) => {
+        evt.preventDefault();
+        axios
+        .post(``)
+        .then(res =>{
+
+        })
+        .catch(err => console.log(err))
+    }
+
+
+
     return(
-        <EditForm>
+        <EditForm onSubmit={Save}>
             <div className="user"><img id="userPic" src={user} alt="user"/></div>
-            <label><h2>Username:</h2>
+            <label className="username"><h2>Username:</h2>
                 <input 
                 type="text"
                 name="username"
@@ -53,14 +67,16 @@ const EditUser = () => {
                 /><img id="pencil" src= {pencil} alt="edit pencil"/>
             </label>
             <br/>
-            <label><h2>Turn notifications on or off:</h2><br/>
-                <input 
+            <label className="notifications"><h2>Turn notifications on or off:</h2><br/>
+                <input
+                id="checkbox"
                 type="checkbox"
                 name="notifications"
                 onChange={Change}
                 value={edit.notifications}
                 />
             </label>
+            <button>Save</button>
         </EditForm>
     )
 }
@@ -71,15 +87,15 @@ flex-direction: column;
 align-items: center;
 
 #pencil {
-    width: 5%;
+    width: 4%;
     height: 3%;
-    border: solid black;
+    /* border: solid black; */
 }
 #userPic {
     width: 25%;
 }
 .user {
-    margin-top: 30%;
+    margin-top: 40%;
     margin-bottom: 20%;
 }
 
@@ -92,10 +108,19 @@ label {
     margin-bottom: -10%;  
 }
 
+.username {
+    display:flex;
+    justify-content: end;
+}
+
+.notifications {
+    margin-top: 19%;
+}
+
 h2 {
     display: flex;
     justify-content: flex-start;
-    font-size:80%;
+    font-size:85%;
     /* border: solid black; */
     width:100%;
 }
@@ -103,8 +128,12 @@ h2 {
 input {
     display: flex;
     flex-direction: right;
-    height: 6%;
+    border:0;
     /* border: solid black; */
+}
+
+button {
+    margin-top: 20%;
 }
 
 `
