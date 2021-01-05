81f4c0d1a771c31ce35f50b8ad8afa8bd646ce1e
Devon Brady <devon.a.brady@gmail.com>
2021-01-05 18:43:35 -0500

    Added edit plant button and plant img tag.

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,40 +3,42 @@ import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 const Dashboard = () => {
-  const user = useSelector((state) => state);
-  const { push } = useHistory();
-  const LogOutButton = () => {
-    localStorage.removeItem("token");
-    push("/login");
-  };
-  return (
-    <div>
-      <div>
-        <button>Edit User</button>
-        <button>Add Plant</button>
-        <button onClick={LogOutButton}>Log Out</button>
-      </div>
-      {/* Make the above button a nav bar  */}
-      <div>
-        <p>Username: {user.username}</p>
-        <p>User Phonenumber: {user.telephone}</p>
-      </div>
-      <div>
-        <h3>Your Plants</h3>
-        {user.plants.map((ele, idx) => {
-          return (
-            <div key={idx}>
-              <p>Name:{ele.nickname}</p>
-              <p>Species:{ele.species}</p>
-              <p>How often to water: Every {ele.frequency}hours</p>
+    const user = useSelector((state) => state);
+    const { push } = useHistory();
+    const LogOutButton = () => {
+        localStorage.removeItem("token");
+        push("/login");
+    };
+    return (
+        <div>
+            <div>
+                <button>Edit User</button>
+                <button>Add Plant</button>
+                <button onClick={LogOutButton}>Log Out</button>
             </div>
+            {/* Make the above button a nav bar  */}
+            <div>
+                <p>Username: {user.username}</p>
+                <p>User Phonenumber: {user.telephone}</p>
+            </div>
+            <div>
+                <h3>Your Plants</h3>
+                {user.plants.map((ele, idx) => {
+                    return (
+                        <div key={idx}>
+                            <img src={ele.img_url} />
+                            <p>Name:{ele.nickname}</p>
+                            <p>Species:{ele.species}</p>
+                            <p>How often to water: Every {ele.frequency}hours</p>
+                            <button>Edit Plant</button>
+                        </div>
 
-            // Style each plant div to be a card for each plant.
-          );
-        })}
-      </div>
-    </div>
-  );
+                        // Style each plant div to be a card for each plant.
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default Dashboard;
